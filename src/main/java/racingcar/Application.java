4b3b4aeb510caa9918578fb30c864080c67654c5
stk346 <stk346@gamil.com>
2022-08-02 22:10:27 +0900

    docs README 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,19 +1,9 @@
 package racingcar;
 
-import racingcar.controller.GameController;
-import racingcar.controller.InputController;
-import racingcar.model.Car;
-
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String userInput = InputController.getUserInput();
-        List<Car> cars = InputController.generateCarList(userInput);
-        int playCount = InputController.getPlayCount();
-        GameController gamecontroller = new GameController(cars, playCount);
-        gamecontroller.playGame();
-        gamecontroller.printWinners();
     }
 }
